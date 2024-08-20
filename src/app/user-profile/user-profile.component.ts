8fa78c03809814f15a57e08c8c6acd94f8e9cc51
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-08-20 22:37:04 +0530

    NG MODULES

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,19 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import   
+ { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,
+    CommonModule
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
-  name:string = "vivek"
-  job:string = " software Engineer"
-  salary:number = 120000
-  isButtonDisabled = true
 
-  onchange(){
-   console.log('called')
-  }
+
+  @Input() name = ""
+
+  // name:string = "vivek"
+  // job:string = " software Engineer"
+  // salary:number = 120000
+  // isButtonDisabled = true
+  // inputVal = "test"
+
+
+  // users = [
+  //   {name:"ramesh",
+  //     isSingle: true,
+  //     salary:10000
+  //   },
+  //   {name:"vicky",
+  //     isSingle: true,
+  //     salary:500000
+  //   },
+  //   {name:"jack",
+  //     isSingle: false,
+  //     salary:100000
+  //   }
+  // ]
+
+  // onchange(e:Event){
+  //   const value = (e.target as HTMLInputElement).value
+  //   this.inputVal = value
+  //  console.log(value)
+  // }
+
 };
